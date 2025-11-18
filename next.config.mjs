cc640a63f2576b83dbe19a4121ffaf271e7f5b78
Vijaypatil20 <88785221+Vijaypatil20@users.noreply.github.com>
2025-11-18 15:55:04 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
-/** @type {import('next').NextConfig} */
+// next.config.mjs or next.config.js
 const nextConfig = {
+  output: "export",   // this makes Next.js do static export
+  basePath: "/Madam_Birthday",  // IMPORTANT: this should match your repo name
+  images: {
+    unoptimized: true   // disable Next.js image optimization for static export
+  }
 };
-
 export default nextConfig;
